837dee9d3b7d2229681ecfe69131eb3bc82e0128
marlitka <93336516+marlitka@users.noreply.github.com>
2023-05-01 13:00:34 -0500

    initial pass (#1)

---
+++ BillingSysAPI/BillingSys/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FunctionApp1.Models
+{
+    public class Subscription
+    {
+        [JsonProperty(PropertyName="id")]
+        public string Id { get; set; }
+        public int Price { get; set; }
+
+        public string PartitionKey { get; set; }
+    }
+}
---
+++ BillingSysAPI/BillingSys/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+namespace FunctionApp1.Models
+{
+    public class Users
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string PartitionKey { get; set; }
+
+        public string SubId { get; set; }
+    }
+}
